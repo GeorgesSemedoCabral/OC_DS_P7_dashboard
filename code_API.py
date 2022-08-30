ed9e542d5dc0e071499a120e223c3d09093c649a
Georges SEMEDO CABRAL <semedocabralg@gmail.com>
2022-08-30 15:54:50 +0200

    many good updates

--- code_API.py
+++ code_API.py
@@ -7,27 +7,40 @@ from pydantic import BaseModel
 app = FastAPI()
 lgbm_model = joblib.load("balanced_lgbm_model.sav")
 
-class ClientIndex(BaseModel):
-    client_index : int
+class ClientID(BaseModel):
+    SK_ID_CURR : int
 
 @app.post("/predict")
-def predict_scoring(client: ClientIndex):
+def profile_and_predict(client: ClientID):
     data = client.dict()
-    df = pd.read_csv("preprocessed_test_df.csv")
-    feats = [f for f in df.columns if f not in [
+    client_df = pd.read_csv("original_test_df.csv")
+    app_client = client_df[client_df["SK_ID_CURR"]==data["SK_ID_CURR"]]
+    if app_client.loc[:, "CODE_GENDER"].to_numpy().item(0) == 0:
+        gender = "M"
+    elif app_client.loc[:, "CODE_GENDER"].to_numpy().item(0) == 1:
+        gender = "F"
+    children = app_client.loc[:, "CNT_CHILDREN"].to_numpy().item(0)
+    test_df = pd.read_csv("preprocessed_test_df.csv")
+    feats = [f for f in test_df.columns if f not in [
         "TARGET",
-        "SK_ID_CURR",
         "SK_ID_BUREAU",
         "SK_ID_PREV",
         "index"
     ]]
-    app_df = df[feats]
-    app_client = app_df.iloc[[data["client_index"]]]
-    score = lgbm_model.predict(app_client)[0]
-    proba = lgbm_model.predict_proba(app_client).max()
+    test_feats = test_df[feats]
+    app_df = test_feats[test_feats["SK_ID_CURR"]==data["SK_ID_CURR"]]
+    app_test = app_df.drop(columns="SK_ID_CURR")
+    if lgbm_model.predict(app_test)[0] == 0:
+        score = "G"
+    elif lgbm_model.predict(app_test)[0] == 1:
+        score = "B"
+    proba = lgbm_model.predict_proba(app_test).item(1)
     return {
-        "score": score,
-        "proba": proba
+        "client_ID": data["SK_ID_CURR"],
+        "gender": gender,
+        "children": children,
+        "SCORE": score,
+        "PROBA": proba
     }
 
 if __name__ == "__main__":
