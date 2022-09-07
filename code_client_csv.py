e73b7434b9b0fbdcc8458f259798339d8625ef5d
Georges SEMEDO CABRAL <semedocabralg@gmail.com>
2022-09-07 11:39:11 +0200

    add preprocessing code for dashboard EDA

---
+++ code_client_csv.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+client = pd.read_csv("../../input/application_test.csv")
+client.fillna("NaN", inplace=True)
+client["DAYS_BIRTH"] = client["DAYS_BIRTH"].apply(
+    lambda x: int(np.abs(x) / 365.25)
+)
+client["ORGANIZATION_TYPE"] = client["ORGANIZATION_TYPE"].apply(
+    lambda x: x.split(":")[0]
+)
+client["ORGANIZATION_TYPE"] = client["ORGANIZATION_TYPE"].apply(
+    lambda x: x.split(" Entity")[0]
+)
+client["AMT_INCOME_TOTAL"] = client["AMT_INCOME_TOTAL"].apply(
+    lambda x: int(x / 73.3670)
+)
+client["AMT_ANNUITY"] = client["AMT_ANNUITY"].astype("float64")
+
+client.to_csv("client_test.csv", index=False)
