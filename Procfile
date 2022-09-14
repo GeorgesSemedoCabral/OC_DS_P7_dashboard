ddd6e9d919e3d760443fd6cd29a4058eff564017
Georges SEMEDO CABRAL <semedocabralg@gmail.com>
2022-09-14 16:03:26 +0200

    update routes for API deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 web: gunicorn code_dashboard:server
+
+web: uvicorn code_API:app --host=0.0.0.0 --port=${PORT:-5000}
--- code_API.py
+++ code_API.py
@@ -86,4 +86,4 @@ def client_features(client: ClientID2):
     }
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=-5000)
