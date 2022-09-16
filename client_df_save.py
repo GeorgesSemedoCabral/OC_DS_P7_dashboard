11ac4d25743269a00621119fd59f711fdacb179f
Georges SEMEDO CABRAL <semedocabralg@gmail.com>
2022-09-17 01:30:39 +0200

    update currency credits and annuities

--- client_df_save.py
+++ client_df_save.py
@@ -16,6 +16,11 @@ client["ORGANIZATION_TYPE"] = client["ORGANIZATION_TYPE"].apply(
 client["AMT_INCOME_TOTAL"] = client["AMT_INCOME_TOTAL"].apply(
     lambda x: int(x / 73.3670)
 )
-client["AMT_ANNUITY"] = client["AMT_ANNUITY"].astype("float64")
+client["AMT_CREDIT"] = client["AMT_CREDIT"].apply(
+    lambda x: int(x / 73.3670)
+)
+client["AMT_ANNUITY"] = client["AMT_ANNUITY"].astype("float64").apply(
+    lambda x: int(x / 73.3670)
+)
 #client.to_csv("data/client_test.csv", index=False)
 joblib.dump(client, "data/client_test.sav")
