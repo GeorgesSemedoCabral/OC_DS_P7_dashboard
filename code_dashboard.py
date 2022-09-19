5e2b409b8d1fc7fc63b0a61eb9bfa4daa5e270a6
Georges SEMEDO CABRAL <semedocabralg@gmail.com>
2022-09-19 23:23:47 +0200

    update to check 2+ filters, none for all clients

--- code_dashboard.py
+++ code_dashboard.py
@@ -43,6 +43,16 @@ client_rows = [
     "Monthly income (€, 05-18-2018)",
     "Credit (€, 05-18-2018)"
 ]
+client_opt = [
+    "CODE_GENDER",
+    "DAYS_BIRTH",
+    "NAME_FAMILY_STATUS",
+    "CNT_CHILDREN",
+    "NAME_HOUSING_TYPE",
+    "NAME_EDUCATION_TYPE",
+    "OCCUPATION_TYPE",
+    "ORGANIZATION_TYPE"
+]
 
 app.layout = html.Div([
     html.Div([
@@ -63,62 +73,26 @@ app.layout = html.Div([
         html.Div(id="table_output")
     ]),
     html.Div([
-        html.H1("Comparison with all clients"),
-        html.H3("Filter by:"),
+        html.H1("Comparison with other clients"),
+        html.H3(
+            "Check to filter similar clients. For all clients, select none."
+        ),
+        dcc.Checklist(
+            id="group_list",
+            options=client_opt,
+            #value=["CODE_GENDER"],
+            inline=True
+        ),
+        html.Br(),
+        html.Div(id="remind_output"),
+        html.H3("Compare clients by:"),
         dcc.Dropdown(
-            id="names_drop",
-            options=[
-                "CODE_GENDER",
-                "DAYS_BIRTH",
-                "NAME_FAMILY_STATUS",
-                "CNT_CHILDREN",
-                "NAME_HOUSING_TYPE",
-                "NAME_EDUCATION_TYPE",
-                "OCCUPATION_TYPE",
-                "ORGANIZATION_TYPE"
-            ],
+            id="filter_drop",
+            options=client_opt,
             value="CODE_GENDER",
             clearable=False
         ),
         dcc.Graph(id="graph_01")
-    ]),
-    html.Div([
-        html.H1("Comparison with similar clients"),
-        html.H3("Group by:"),
-        dcc.Dropdown(
-            id="group_drop",
-            options=[
-                "CODE_GENDER",
-                "DAYS_BIRTH",
-                "NAME_FAMILY_STATUS",
-                "CNT_CHILDREN",
-                "NAME_HOUSING_TYPE",
-                "NAME_EDUCATION_TYPE",
-                "OCCUPATION_TYPE",
-                "ORGANIZATION_TYPE"
-            ],
-            value="CODE_GENDER",
-            clearable=False
-        ),
-        html.Br(),
-        html.Div(id="remind_output"),
-        html.H3("Filter by:"),
-        dcc.Dropdown(
-            id="filter_drop",
-            options=[
-                "CODE_GENDER",
-                "DAYS_BIRTH",
-                "NAME_FAMILY_STATUS",
-                "CNT_CHILDREN",
-                "NAME_HOUSING_TYPE",
-                "NAME_EDUCATION_TYPE",
-                "OCCUPATION_TYPE",
-                "ORGANIZATION_TYPE"
-            ],
-            value="CODE_GENDER",
-            clearable=False
-        ),
-        dcc.Graph(id="graph_02")
     ])
 ])
 
@@ -209,89 +183,12 @@ def generate_table(id_input, n_clicks):
 
 @app.callback(
     Output("graph_01", "figure"),
-    Input("names_drop", "value"),
-    Input("id_input", "value"),
-    Input("input_button", "n_clicks")
-)
-def generate_figure_all(names_drop, id_input, n_clicks):
-    if n_clicks == 0:
-        raise exceptions.PreventUpdate
-    else:
-        if id_input is None:
-            raise exceptions.PreventUpdate
-        if id_input not in list(client["SK_ID_CURR"]):
-            raise exceptions.PreventUpdate
-        client_ID2 = {"SK_ID_CURR": id_input}
-        id_client = client[client["SK_ID_CURR"]==client_ID2["SK_ID_CURR"]]
-        x = list(client[names_drop].unique())
-        client_x = x.index(id_client[names_drop].values)
-        fig = make_subplots(
-            rows=1,
-            cols=2,
-            subplot_titles=[
-                "Groups distribution",
-                "Monthly incomes and credits (€, 05-18-2018)"
-            ],
-            specs=[[{"type": "pie"}, {"type": "xy"}]]
-        )
-        pull = [0, ] * len(x)
-        pull[client_x] = 0.2
-        fig.add_trace(
-            go.Pie(
-                labels=x,
-                values=list(client[names_drop].value_counts(sort=False)),
-                hoverinfo="label+value+percent",
-                textinfo="percent",
-                textposition="inside",
-                pull=pull
-            ),
-            row=1,
-            col=1
-        )
-        income_color = ["lightblue", ] * len(x)
-        credit_color = ["lightpink", ] * len(x)
-        income_color[client_x] = "blue"
-        credit_color[client_x] = "red"
-        fig.add_trace(
-            go.Bar(
-                x=x,
-                y=list(client.groupby(names_drop, sort=False)
-                       ["AMT_INCOME_TOTAL"].mean()),
-                name="Monthly income",
-                marker_color=income_color
-            ),
-            row=1,
-            col=2
-        )
-        fig.add_trace(
-            go.Bar(
-                x=x,
-                y=list(client.groupby(names_drop, sort=False)
-                       ["AMT_CREDIT"].mean()),
-                name="Credit",
-                marker_color=credit_color
-            ),
-            row=1,
-            col=2
-        )
-        fig.update_layout(
-            #uniformtext_minsize=12,
-            #uniformtext_mode="hide",
-            height=500,
-            width=1500,
-            template="simple_white",
-            showlegend=False
-        )
-        return fig
-
-@app.callback(
-    Output("graph_02", "figure"),
-    Input("group_drop", "value"),
+    Input("group_list", "value"),
     Input("filter_drop", "value"),
     Input("id_input", "value"),
     Input("input_button", "n_clicks")
 )
-def generate_figure_sim(group_drop, filter_drop, id_input, n_clicks):
+def generate_figure(group_list, filter_drop, id_input, n_clicks):
     if n_clicks == 0:
         raise exceptions.PreventUpdate
     else:
@@ -301,14 +198,18 @@ def generate_figure_sim(group_drop, filter_drop, id_input, n_clicks):
             raise exceptions.PreventUpdate
         client_ID2 = {"SK_ID_CURR": id_input}
         id_client = client[client["SK_ID_CURR"]==client_ID2["SK_ID_CURR"]]
-        group = client[client[group_drop].values==id_client[group_drop].values]
+        group = client
+        if group_list is not None:
+            for i in range(len(group_list)):
+                group = (group[group[group_list[i]].values
+                         ==id_client[group_list[i]].values])
         x = list(group[filter_drop].unique())
         client_x = x.index(id_client[filter_drop].values)
         fig = make_subplots(
             rows=1,
             cols=2,
             subplot_titles=[
-                "Sub-groups distribution",
+                "Groups distribution",
                 "Monthly incomes and credits (€, 05-18-2018)"
             ],
             specs=[[{"type": "pie"}, {"type": "xy"}]]
@@ -365,11 +266,11 @@ def generate_figure_sim(group_drop, filter_drop, id_input, n_clicks):
 
 @app.callback(
     Output("remind_output", "children"),
-    Input("group_drop", "value"),
+    Input("group_list", "value"),
     Input("id_input", "value"),
     Input("input_button", "n_clicks")
 )
-def generate_reminder(group_drop, id_input, n_clicks):
+def generate_reminder(group_list, id_input, n_clicks):
     if n_clicks == 0:
         raise exceptions.PreventUpdate
     else:
@@ -377,11 +278,15 @@ def generate_reminder(group_drop, id_input, n_clicks):
             raise exceptions.PreventUpdate
         if id_input not in list(client["SK_ID_CURR"]):
             raise exceptions.PreventUpdate
+        if group_list is None:
+            raise exceptions.PreventUpdate
         client_ID2 = {"SK_ID_CURR": id_input}
         id_client = client[client["SK_ID_CURR"]==client_ID2["SK_ID_CURR"]]
         df = pd.DataFrame({
-            "GROUP (similar clients)" : group_drop,
-            "CLIENT INFO (for recall)" : id_client[group_drop].values
+            "GROUP (similar clients)" : group_list,
+            "CLIENT INFO (for recall)" : list(
+                id_client.loc[:, i].to_numpy().item(0) for i in group_list
+            )
         })
         return DataTable(
             data=df.to_dict("records"),
