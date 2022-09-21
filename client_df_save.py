9df618c459281fb173736b4c96affb8ce086f7fd
Georges SEMEDO CABRAL <semedocabralg@gmail.com>
2022-09-21 20:20:23 +0200

    add years and child binning

--- client_df_save.py
+++ client_df_save.py
@@ -8,6 +8,22 @@ client["DAYS_BIRTH"] = client["DAYS_BIRTH"].apply(
     lambda x: int(np.abs(x) / 365.25)
 )
 client.rename(columns={"DAYS_BIRTH": "YEARS_BIRTH"}, inplace=True)
+age_bins = [17, 24, 34, 44, 54, 64, 74]
+age_labels = ["25-", "25-34", "35-44", "45-54", "55-64", "65+"]
+client["YEARS_CLASS"] = pd.cut(
+    client["YEARS_BIRTH"],
+    age_bins,
+    labels=age_labels,
+    ordered=False
+).astype(str)
+child_bins = [-1, 0, 1, 2, 20]
+child_labels = ["0", "1", "2", "3+"]
+client["CHILD_CLASS"] = pd.cut(
+    client["CNT_CHILDREN"],
+    child_bins,
+    labels=child_labels,
+    ordered=False
+).astype(str)
 client["ORGANIZATION_TYPE"] = client["ORGANIZATION_TYPE"].apply(
     lambda x: x.split(":")[0]
 )
@@ -20,4 +36,5 @@ client["AMT_INCOME_TOTAL"] = client["AMT_INCOME_TOTAL"].apply(
 client["AMT_CREDIT"] = client["AMT_CREDIT"].apply(
     lambda x: int(x / 73.3670)
 )
+
 joblib.dump(client, "data/client_test.sav")
